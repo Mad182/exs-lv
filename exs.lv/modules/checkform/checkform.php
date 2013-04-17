14e35c4983ad1d3d034f2d5dd54408cd426fc93d
Edgars <edgar.pelna@gmail.com>
2013-04-17 18:29:37 +0300

    Shaadi taadi siikumi un precizitaates atrastajos datos, like, bana ilgums banotajiem profiliem.

--- exs.lv/modules/checkform/checkform.php
+++ exs.lv/modules/checkform/checkform.php
@@ -4,7 +4,7 @@ if (!im_mod()) {
 	redirect();
 }
 
-// jquery ielādē izvēlētā lietotāja vecos nikus
+// jquery ielādē izvēlētā lietotāja vecos nikus, profilus, kuriem sakrīt parole, un bana termiņus
 if (isset($_GET['display']) && is_numeric($_GET['display'])) {
 
 	$userid = (int) $_GET['display'];
@@ -15,40 +15,53 @@ if (isset($_GET['display']) && is_numeric($_GET['display'])) {
 
 		// veic salīdzināšanu, vai paroles hash sakrīt ar kādu citu
 		if (strlen($user->pwd) > 5 && !in_array($user->pwd, array('', ' '))) {
-			$pass = $db->get_results("SELECT `id`,`nick`,`lastseen`,`level`,`lastip` FROM `users` WHERE `pwd` LIKE '%" . $user->pwd . "%' AND `id` != '" . $user->id . "' AND `id` != '".$auth->id."' ORDER BY `nick` ASC ");
+		
+			$pass = $db->get_results("SELECT `id`,`nick`,`lastseen`,`level`,`lastip` FROM `users` WHERE `pwd` LIKE '%" .$user->pwd . "%' AND `id` != '" . $user->id . "' AND `id` != '".$auth->id."' ORDER BY `nick` ASC ");
+			
 			if ($pass) {
+			
 				$content .= '<p class="infop"><strong>Parole sakrīt ar šādiem lietotājiem:</strong></p><p>';
-				$content .= '<table><tr><th>Profils</th><th>Manīts</th><th>Pēdējā IP</th></tr>';
+				$content .= '<table><tr><th>Profils</th><th>Manīts</th><th>Pēdējā IP</th><th>Bans vēl...</th></tr>';
+				
 				foreach ($pass as $pwd) {
+				
+					// pārbauda, vai šim profilam ir aktīvs bans
+					$active_ban = $db->get_row("SELECT `time`,`length` FROM `banned` WHERE `user_id` = '".$pwd->id."' ORDER BY `time` DESC ");
+					
+					if ($active_ban) {
+						$len = time() - $active_ban->time;					
+						if ($len < $active_ban->length) {
+							$banned = '<strong>'.floor(($active_ban->length - $len)/60/60/24).'</strong> dienas';
+						} else $banned = ' -- ';
+					} else $banned = ' -- ';
+					
 					$pwd->lastseen = time_ago(strtotime($pwd->lastseen));
 					$pwd->nick = usercolor($pwd->nick, $pwd->level, false, $pwd->id);
-					$content .= '<tr><td><a href="/user/'.$pwd->id.'">'.$pwd->nick.'</a></td><td class="centered-result">pirms ' . $pwd->lastseen . '</td><td class="centered-result">'.$pwd->lastip.'</td></tr>';
+					$content .= '<tr><td><a href="/user/'.$pwd->id.'">'.$pwd->nick.'</a></td><td class="centered-result">pirms ' . $pwd->lastseen . '</td><td class="centered-result">'.$pwd->lastip.'</td><td class="centered-result">'.$banned.'</td></tr>';
 				}
 				$content .= '</table></p>';
 			} else
 				$content .= '<p class="infop"><strong>Parole ne ar vienu lietotāju nesakrīt.</strong></p>';
 
-			// jāpārbauda vecā parole
 		} else {
 			$content .= '<p class="infop"><strong>Parole ne ar vienu lietotāju nesakrīt.</strong></p>';
 		}		
 		
 		// atrod vecos lietotājvārdus
-		$usernames = $db->get_results("SELECT `user_id` AS `id`,`nick`,`changed` FROM `nick_history` WHERE `user_id` = '" . $user->id . "' ORDER BY `changed` DESC ");
-		if ($usernames) {
+		$usernames = $db->get_results("SELECT `user_id` AS `id`,`nick`,`changed` FROM `nick_history` WHERE `user_id` = '" . $user->id . "' ORDER BY `changed` DESC ");		
+		if ($usernames) {		
 			$content .= '<p class="infop"><strong>Iepriekšējie lietotājvārdi:</strong></p><p>';
 			foreach ($usernames as $uname) {
 				$uname->changed = date("d.m.Y, H:i", strtotime($uname->changed));
 				$content .= '<a href="/user/' . $uname->id . '">' . $uname->nick . '</a> &nbsp;&nbsp;&nbsp;(mainīts: ' . $uname->changed . ')<br />';
 			}
-			$content .= '</p>';
+			$content .= '</p>';			
 		} else {
 			$content .= '<p class="infop" style="font-weight:bold;">Šis lietotājs savu lietotājvārdu pēdējā laikā nav mainījis!</p>';
 		}
 
 		echo $content;
 	}
-
 	exit;
 }
 
