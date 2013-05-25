6cb5bdfabefda823f47f5a30aaedec121ae6cf50
Madars <mad182@gmail.com>
2013-05-25 13:20:30 +0300

    err

--- exs.lv/modules/forums/forums.php
+++ exs.lv/modules/forums/forums.php
@@ -89,7 +89,7 @@ if (!empty($cats)) {
 		$forums = $db->get_results("SELECT `title`, `textid`, `icon`, `id`, `content`, `stat_topics`, `stat_com`, `mods_only_post`, `status` FROM `cat` WHERE `parent` = '$cat->id' AND `module` = 'list'" . $add . " ORDER BY `ordered` ASC");
 
 		foreach ($forums as $forum) {
-			if (!$forum->mods_only_post || im_mod() && $cat->status == 'active') {
+			if (!$forum->mods_only_post || im_mod() && $forum->status == 'active') {
 				$fcategorys[] = array(
 					'id' => $forum->id,
 					'title' => $forum->title,
