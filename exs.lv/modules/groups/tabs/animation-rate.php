6d70c855f7f2f6a0a5e9a66471f566de76056228
Madars <mad182@gmail.com>
2013-07-07 22:02:18 +0300

    Movie rater tabs update, moved to single file

---
+++ exs.lv/modules/groups/tabs/animation-rate.php
@@ -0,0 +1,6 @@
+<?php
+
+$movie_rater_type = 'animation';
+$movie_rater_description = '<p style="font-size:90%;">Lūdzu nobalso par animācijas filmām, kuras esi redzējis. Ja Tev filma patika un iesaki to citiem, spied "Man patīk", bet ja skatījies un uzskati, ka nav vērts tērēt laiku, spied "Man nepatīk". Balsot var sasnidzot 100. karmas līmeni. Tavi vērtējumi būs redzami citiem.</p>';
+
+require(CORE_PATH . '/modules/groups/tabs/movie-rater-inc.php');
