f67a2ea3be86ab2fc2a9cd397958c4fb43343dde
Madars <mad182@gmail.com>
2013-07-12 15:42:22 +0300

    ID ir bezjēdzīgi

--- exs.lv/modules/groups/tabs/pikseli.php
+++ exs.lv/modules/groups/tabs/pikseli.php
@@ -17,18 +17,18 @@ sort($files, SORT_NUMERIC);
 $ti = 0;
 foreach($files as $file) {
 	if($ti == 0) {
-	$module_content .= '<tr>';
+		$module_content .= '<tr>';
 	}
 	
 	$title = str_replace('.jpg','',$file);
 	list($id, $title) = explode('-', $title);
 	
 	$module_content .= '<td>';
-	$module_content .= '<img src="http://img'.alternator(1,2,0).'.exs.lv/pikseli-atbildes/'.$file.'" alt="'.$title.'" title="'.$id.'. līmenis - '.$title.'" style="width:240px;" />';
-	$module_content .= '<p style="text-align: center;">'.$id.'. <strong>'.ucfirst($title).'</strong></p>';
+	$module_content .= '<img src="http://img'.alternator(1,2,0).'.exs.lv/pikseli-atbildes/'.$file.'" alt="'.$title.'" title="'.$title.'" style="width:240px;" />';
+	$module_content .= '<p style="text-align: center;"><strong>'.ucfirst($title).'</strong></p>';
 	$module_content .= '</td>';
 	if($ti == 2) {
-	$module_content .= '</tr>';
+		$module_content .= '</tr>';
 	}
 	$ti++;
 	if($ti > 2) {
@@ -37,4 +37,3 @@ foreach($files as $file) {
 }
 
 $module_content .= '</table>';
-
