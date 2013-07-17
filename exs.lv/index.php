b0e2d38c132b4f051444c601cdbe1c160ccf7819
Madars <mad182@gmail.com>
2013-07-17 09:29:12 +0300

    Smartad code

--- exs.lv/index.php
+++ exs.lv/index.php
@@ -438,6 +438,11 @@ if ($skin === 'main') {
 		$tpl->newBlock('async-call');
 	}*/
 
+	/* smartad.eu */
+	if($lang == 1 && $auth->karma < 1000) {
+		$tpl->newBlock('smartad-eu');
+	}
+
 }
 
 /* robots meta taga pievienošana */
