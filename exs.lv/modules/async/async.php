4cb15a37b4da24a6df9de5422ebe68159fa6f4d5
Madars <madars@mendo.lv>
2013-08-01 16:13:19 +0300

    wos tops

--- exs.lv/modules/async/async.php
+++ exs.lv/modules/async/async.php
@@ -5,12 +5,9 @@
 if(isset($_GET['site'])) {
 
 	if($_GET['site'] == 'mta') {
-		die('<html><body><script src=http://wos.lv/c.php?16006></script></body></html>');
+		die('<html><body><script src=http://wos.lv/c.php?27967></script></body></html>');
 	}
 
-	if($_GET['site'] == 'exs') {
-		die('<html><body><script src=http://wos.lv/c.php?26271></script></body></html>');
-	}
 }
 
 if(!$db->get_var("SELECT count(*) FROM `async_ip` WHERE `ip` = '$auth->ip'")) {
@@ -19,7 +16,6 @@ if(!$db->get_var("SELECT count(*) FROM `async_ip` WHERE `ip` = '$auth->ip'")) {
 
 	die('<html><body>
 		<iframe src="http://rp.exs.lv/async?site=mta"></iframe>
-		<iframe src="http://exs.lv/async?site=exs"></iframe>
 	</body></html>');
 
 }
