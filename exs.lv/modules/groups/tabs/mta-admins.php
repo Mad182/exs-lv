bd7bc38c682865ac8591a19479750d2e7e7587ec
Madars Anziķis <mad182@gmail.com>
2013-08-26 17:10:34 +0300

    MTA admins

--- exs.lv/modules/groups/tabs/mta-admins.php
+++ exs.lv/modules/groups/tabs/mta-admins.php
@@ -1,8 +1,9 @@
 <?php
 
 $mtadb = new mdb($mta_username, $mta_password, $mta_database, $mta_hostname);
+$mtadb->query("SET NAMES utf8");
 
-$admins = $mtadb->get_results("SELECT `username`,`admin`,`adminreports`,`lastlogin` FROM `accounts` WHERE `admin` != 0 ORDER BY `admin` DESC");
+$admins = $mtadb->get_results("SELECT `exs_id`, `username`, `admin`, `adminreports`, `lastlogin`, `responsibleFor` FROM `accounts` WHERE `admin` != 0 ORDER BY `admin` DESC");
 
 if(!empty($admins)) {
 
@@ -11,18 +12,28 @@ if(!empty($admins)) {
 	<table class="main-table">
 		<tr>
 			<th>Admins</th>
+			<th>Exs niks</th>
 			<th>Līmenis</th>
 			<th>Reporti</th>
 			<th>Pēdējo reizi redzēts</th>
+			<th>Atbildīgs par</th>
 		</tr>
 	';
 
 	foreach($admins as $admin) {
+
+		$usr = '';
+		if($exs = get_user($admin->exs_id)) {
+			$usr = '<a href="/user/' . $exs->id . '">' . usercolor($exs->nick, $exs->level, false, $exs->id) . '</a>';
+		}
+
 		$module_content .= '<tr>';
 		$module_content .= '	<td>' . $admin->username . '</td>';
+		$module_content .= '	<td>' . $usr . '</td>';
 		$module_content .= '	<td>' . $admin->admin . '</td>';
 		$module_content .= '	<td>' . $admin->adminreports . '</td>';
 		$module_content .= '	<td>' . $admin->lastlogin . '</td>';
+		$module_content .= '	<td>' . $admin->responsibleFor . '</td>';
 		$module_content .= '</tr>';
 	}
 	
