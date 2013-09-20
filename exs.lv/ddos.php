0fe68f9e2b7dcbe14ab68d21abe8a94a32c52139
Madars Anziķis <mad182@gmail.com>
2013-09-21 01:04:57 +0300

    Skripts kas bloķē ip adreses ar pārāk daudz konekcijām

---
+++ exs.lv/ddos.php
@@ -0,0 +1,63 @@
+<?php
+
+if (PHP_SAPI !== 'cli') {
+	echo 'CLI only!';
+	exit;
+}
+
+echo 'ddos.php started' . "\n";
+
+chdir(__DIR__);
+ini_set('memory_limit', '256M');
+error_reporting(0);
+ini_set('display_errors', 'Off');
+require('configdb.php');
+require(CORE_PATH . '/includes/class.mdb.php');
+require(CORE_PATH . '/includes/functions.core.php');
+
+$debug = true;
+
+
+$list = `netstat -atun | awk '{print $5}' | cut -d: -f1 | sed -e '/^$/d' |sort | uniq -c | sort -n`;
+
+$lines = explode("\n", $list);
+
+$whitelist = array('127.0.0.1', '127.0.0.2', '0.0.0.0');
+$blocked = array();
+
+foreach($lines as $line) {
+
+	preg_match('#([0-9]+) ([0-9]+.[0-9]+.[0-9]+.[0-9]+)#i', $line, $matches);
+
+	if($matches[1] > 300 && !in_array($matches[2], $whitelist)) {
+		$com = "ufw insert 1 deny from ".$matches[2];
+		$block = `$com`;
+		$blocked[] = array('ip' => $matches[2], 'conn' => $matches[1]);
+	}
+
+}
+
+if(!empty($blocked)) {
+	require_once(LIB_PATH . '/swiftmailer/lib/swift_required.php');
+
+	$text = '<p>Bloķētas adreses:</p><p>';
+
+	foreach($blocked as $addr) {
+		$text .= $addr['ip'] . ' ('.$addr['conn'].')<br />';
+	}
+
+	$text .= '</p><p>Laiks: '.date('Y-m-d H:i:s').'</p>';
+
+	$transport = Swift_SmtpTransport::newInstance($smtp_hostname, $smtp_port, $smtp_encryption)->setUsername($smtp_account)->setPassword($smtp_password);
+
+	$mailer = Swift_Mailer::newInstance($transport);
+	$message = Swift_Message::newInstance();
+	$message->setSubject('bloķētas ip adreses');
+	$message->setFrom(array('info@exs.lv' => 'Exs.lv community'));
+	$message->setTo('mad182@gmail.com');
+	$message->setBody($text);
+	$message->setContentType("text/html");
+	$mailer->send($message);
+
+}
+
