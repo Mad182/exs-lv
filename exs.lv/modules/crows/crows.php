fdaaef6c864ec5ff4261f0fb5543728453e670bf
edgarspelna <edgar.pelna@gmail.com>
2013-11-05 20:44:05 +0200

    Brīdinājumu kopskata sadaļa

---
+++ exs.lv/modules/crows/crows.php
@@ -0,0 +1,97 @@
+<?php
+/**	
+ *	Pārskatāms pēdējo likto brīdinājumu saraksts
+ *	un ar tiem saistīta informācija.
+ *
+ *	Moduļa adrese: 		exs.lv/crows
+ *	Pēdējās izmaiņas: 	05.10.2013 ( Edgars )
+ */
+
+// ne-moderatorus sūtām prom
+if ( !im_mod() ) {
+	set_flash('Error 403: Permission denied!');
+	redirect();
+}
+
+//$tpl->assignInclude('module-head', CORE_PATH . '/modules/' . $category->module . '/head.tpl');
+//$tpl->prepare();
+$tpl_options = 'no-right';
+$tpl->assign('page-content-title', 'Lietotāju brīdinājumi');
+
+
+
+$warns = $db->get_results("
+	SELECT
+		`warns`.`reason`		AS `warn_reason`,
+		`warns`.`active`		AS `warn_active`,
+		`warns`.`remove_reason`	AS `warn_removal_reason`,
+		`warns`.`removed_by`	AS `warn_removed_by`,
+		`warns`.`created`		AS `warn_created_at`,
+		
+		`offender`.`id`			AS `offender_id`,
+		`offender`.`nick`		AS `offender_nick`,
+		`offender`.`level`		AS `offender_level`,
+		
+		`warned_by`.`id`		AS `creator_id`,
+		`warned_by`.`nick`		AS `creator_nick`,
+		`warned_by`.`level`		AS `creator_level`,
+		
+		IFNULL(`removed_by`.`id`,0) AS `removed_id`,
+		`removed_by`.`nick` 	AS `removed_nick`,
+		`removed_by`.`level` 	AS `removed_level`
+		
+	FROM `warns`
+		JOIN `users` AS `offender` 			ON `warns`.`user_id` 	= `offender`.`id`
+		JOIN `users` AS `warned_by` 		ON `warns`.`created_by` = `warned_by`.`id`
+		LEFT JOIN `users` AS `removed_by` 	ON `warns`.`removed_by` = `removed_by`.`id`
+	ORDER BY 
+		`warns`.`created` DESC 
+	LIMIT 0,100
+");
+if ( !$warns ) {
+	$tpl->newBlock('no-warns-found');
+}
+else {
+
+	$counter = 1;
+
+	$tpl->newBlock('warns-list');
+	
+	foreach ($warns as $warn) {
+	
+		//$warn->warn_created_at = date('d.m H:i', strtotime($warn->warn_created_at) );
+		$warn->warn_created_at = display_time_simple( strtotime($warn->warn_created_at) );
+	
+		// sodītais lietotājs
+		$warn->offender_nick = usercolor($warn->offender_nick, $warn->offender_level);
+		$warn->offender_nick = '<a href="'.mkurl('user', $warn->offender_id, $warn->offender_nick).'">'.$warn->offender_nick.'</a>';
+
+		// soda uzlicējs
+		$warn->creator_nick = usercolor($warn->creator_nick, $warn->creator_level);
+		$warn->creator_nick = '<a href="'.mkurl('user', $warn->creator_id, $warn->creator_nick).'">'.$warn->creator_nick.'</a>';
+		
+		// ja kāds brīdinājumu noņēmis, 
+		// tiek apstrādāts noņēmēja niks
+		if ( $warn->removed_id != '0' ) {
+			$warn->removed_nick = usercolor($warn->removed_nick, $warn->removed_level);
+			$warn->removed_nick = '<a href="'.mkurl('user', $warn->removed_id, $warn->removed_nick).'">'.$warn->removed_nick.'</a>';
+		}
+		else $warn->removed_nick = '';
+		
+		
+		$tpl->newBlock('single-warn');	
+		$tpl->assignAll($warn);
+		$tpl->assign('row_counter', $counter);
+		
+		// ja brīdinājums ticis noņemts...
+		if ( $warn->removed_id != '0' ) {
+		
+			$warn->warn_removal_reason = '<strong>Noņemšanas iemesls:</strong> ( '.$warn->removed_nick.' ) '.$warn->warn_removal_reason;
+		
+			$tpl->assign('removal-reason', $warn->warn_removal_reason);
+			$tpl->assign('removed-warn', ' class="removed_warn" title="Brīdinājums noņemts!"');
+		}
+		
+		$counter++;		
+	}
+}
---
+++ exs.lv/modules/crows/crows.tpl
@@ -0,0 +1,33 @@
+<h1 style="position:relative;top:-15px">Pēdējās brīvībā palaistās vārnas...</h1>
+<!-- START BLOCK : warns-list -->
+<table id="list_warns" class="rslist">
+<tr class="">
+	<th style="width:3%">&nbsp;</td>
+	<th style="width:18%">Iesaistītie</th>
+	<th style="width:63%">Iemesls</th>
+	<th style="width:16%">Laiks</th>
+</tr>
+<!-- START BLOCK : single-warn -->
+<tr{removed-warn}>
+	<td class="counter">{row_counter}</td>
+	<td>
+		<ul class="warn-users">
+			<li><span class="stronger" style="margin-left:11px">No:</span>{creator_nick}</li>
+			<li><span class="stronger">Kam:</span>{offender_nick}</li>
+		</ul>
+	</td>
+	<td>{warn_reason} {removal-reason}</td>
+	<td style="padding-left:0;padding-right:0">
+		<ul class="warn-users">
+			<li>{warn_created_at}</li>
+			<li class="view_more"><a href="/warns/{offender_id}">Skatīt vairāk</a></li>
+		</ul>
+	</td>
+</tr>
+<!-- END BLOCK : single-warn -->
+</table>
+<!-- END BLOCK : warns-list -->
+
+<!-- START BLOCK : no-warns-found -->
+<p>Nav neviena izteikta brīdinājuma! Vai tā var būt?</p>
+<!-- END BLOCK : no-warns-found -->
