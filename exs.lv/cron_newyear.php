b28c89fab0b9d636dced25ebd5811ae4308d08a8
Madars <mad182@gmail.com>
2014-01-01 11:17:09 +0200

    New years award

---
+++ exs.lv/cron_newyear.php
@@ -0,0 +1,29 @@
+<?php
+
+if (PHP_SAPI !== 'cli') {
+	echo 'CLI only!';
+	exit;
+}
+
+echo 'cron_newyearh.php started' . "\n";
+
+chdir(__DIR__);
+ini_set('memory_limit', '256M');
+error_reporting(0);
+ini_set('display_errors', 'On');
+
+require('configdb.php');
+require(CORE_PATH . '/includes/class.mdb.php');
+require('includes/functions.core.php');
+
+//mysql konekcija
+$db = new mdb($username, $password, $database, $hostname);
+unset($password);
+
+//memcached konekcija
+$m = new Memcache;
+$m->connect($mc_host, $mc_port);
+
+$db->query("UPDATE users SET year_first = 1 WHERE id IN(SELECT DISTINCT(author) FROM `miniblog` WHERE `date` LIKE '%-01-01 00:00:%')");
+$db->query("UPDATE users SET year_first = 1 WHERE id IN(SELECT DISTINCT(author) FROM `comments` WHERE `date` LIKE '%-01-01 00:00:%')");
+
