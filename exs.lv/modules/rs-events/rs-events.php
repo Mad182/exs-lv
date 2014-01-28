8c95dd1c0bb7192e40799269c368ea0a6aabb1ce
edgarspelna <edgar.pelna@gmail.com>
2014-01-28 22:19:36 +0200

    rs: pamats vēlāk veidojamai pasākumu sadaļai

---
+++ exs.lv/modules/rs-events/rs-events.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *  RuneScape notikumi, pasākumi, saraksts ar aktivitātēm,
+ *  kas atjaunojas noteiktā laikā.
+ *
+ *  Lapas sānu kolonnā tiek parādīts saraksts ar drīzākajiem notikumiem.
+ *
+ * 	Moduļa adrese: exs.lv/events
+ */
+
+
