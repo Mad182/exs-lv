8e796ae6202851ed44b365c5967564a243ff819f
edgarspelna <edgar.pelna@gmail.com>
2014-01-31 22:18:31 +0200

    rs: faktu pārvaldības sadaļa

--- exs.lv/modules/rs-facts/rs-facts.php
+++ exs.lv/modules/rs-facts/rs-facts.php
@@ -1,34 +1,174 @@
 <?php
-
 /**
- *  RuneScape random fakti;
- *  Šo izsauc jquery.
+ *  RuneScape faktu pārvaldība.
  *
- * 	Moduļa adrese: exs.lv/rsfacts
+ *  Iespēja apskatīt esošos un pievienot jaunus faktus.
+ *
+ * 	@addr   exs.lv/rsfacts
  */
 if ($lang != 9) {
-	die();
+    set_flash('Pieeja sadaļai liegta!');
+	redirect();
+}
+// 3217 - Mahjarrat
+if (!im_mod() && !in_array($auth->id, array(3217))) {
+    set_flash('Pieeja sadaļai liegta!');
+	redirect();
 }
 
 
-// atgriež kādu runescape faktu jquery pieprasījumam;
-// fakts tiek izdrukāts virs lapas banera
+
+/**
+ *  Atgriež RuneScape faktu jquery pieprasījumam parādīšanai sākumlapā
+ */
 if (isset($_GET['_'])) {
 
-	$facts_count = $db->get_var("SELECT count(*) FROM `facts_rs` ");
+	$facts_count = $db->get_var("SELECT count(*) FROM `rs_facts` WHERE `deleted_by` = 0 ");
+    
 	if ($facts_count > 0) {
 
 		$rand = rand(0, $facts_count - 1);
-		$single_fact = $db->get_row("SELECT `text` FROM `facts_rs` LIMIT $rand, 1");
+		$single_fact = $db->get_row("SELECT `text` FROM `rs_facts` WHERE `deleted_by` = 0 LIMIT $rand, 1");
 
 		if ($single_fact) {
-			echo json_encode(array('state' => 'success', 'content' => strip_tags($single_fact->text)));
-		} else {
-			echo json_encode(array('state' => 'error', 'content' => 'Neizdevās atlasīt nevienu RuneScape faktu! ;('));
+			echo json_encode(array('state' => 'success', 'content' => $single_fact->text));
+            exit;
 		}
-	} else {
-		echo json_encode(array('state' => 'error', 'content' => 'Neizdevās atlasīt nevienu RuneScape faktu! ;('));
 	}
+    echo json_encode(array('state' => 'error', 'content' => 'Piedod, neatradu nevienu RuneScape faktu! ;('));
+    exit;
 }
 
-exit;
+
+
+// fakta dzēšana
+else if (isset($_GET['var1']) && $_GET['var1'] == 'delete' && 
+         isset($_GET['var2']) && is_numeric($_GET['var2'])) {
+         
+    if (!isset($_GET['val']) || $_GET['val'] != $auth->xsrf) {
+        set_flash('Pls, no!');
+        redirect('/' . $category->textid);
+    } 
+    
+	$delete = (int)$_GET['var2'];
+	if ($db->query("
+        UPDATE `rs_facts` 
+        SET 
+            `deleted_by` = '".$auth->id."', 
+            `deleted_at` = '".time()."' 
+        WHERE `id` = $delete 
+        LIMIT 1"
+    )) {
+        set_flash('Fakts veiksmīgi dzēsts!');
+    } else {
+        set_flash('Faktu dzēst neizdevās!');
+    }
+	redirect('/' . $category->textid);
+}
+
+
+
+// fakta rediģēšana
+else if (isset($_GET['var1']) && $_GET['var1'] == 'edit' && 
+         isset($_GET['var2']) && is_numeric($_GET['var2'])) {
+
+	$fact_id    = (int)$_GET['var2'];
+	$fact       = $db->get_row("SELECT * FROM `rs_facts` WHERE `deleted_by` = 0 AND `id` = $fact_id ");
+
+	// tukšu lapu nav vērts rādīt, tāpēc pārvirzām uz faktu sarakstu
+	if (!$fact) {
+        set_flash('Kļūdaini norādīts fakta ID!');
+		redirect('/' . $category->textid);
+	}
+
+	// fakta informācijas atjaunošana datubāzē
+	if (isset($_POST['edit-fact'])) {
+    
+        if (!isset($_POST['anti-xsrf']) || $_POST['anti-xsrf'] != $auth->xsrf) {
+            set_flash('Pls, no!');
+            redirect('/' . $category->textid);
+        }
+
+		$fact_text = sanitize(trim($_POST['edit-fact']));
+
+		if ($db->query("
+            UPDATE `rs_facts` 
+            SET 
+                `text`          = '$fact_text',
+                `updated_by`    = '".(int)$auth->id."',
+                `updated_at`    = '".time()."'
+            WHERE `id` = $fact_id 
+        ")) {
+            set_flash('Fakts veiksmīgi labots!');
+		} else {
+            set_flash('Faktu izlabot neizdevās!');
+        }
+		redirect('/' . $category->textid);
+	}
+
+	// rediģēšanas forma
+    $fact->text = stripslashes($fact->text);
+	$tpl->newBlock('block-edit');
+	$tpl->assignAll($fact);
+    $tpl->assign('xsrf', $auth->xsrf);
+}
+
+
+
+// fakta ierakstīšana datubāzē
+else if (isset($_POST['new-fact'])) {
+
+    if (!isset($_POST['anti-xsrf']) || $_POST['anti-xsrf'] != $auth->xsrf) {
+        set_flash('Pls, no!');
+        redirect('/' . $category->textid);
+    }
+    
+	$newfact = sanitize(trim($_POST['new-fact']));
+
+	if ($db->query("
+        INSERT INTO `rs_facts` 
+            (text, created_by, created_at) 
+        VALUES 
+            ('$newfact', '".(int)$auth->id."', '".time()."')
+    ")) {
+		set_flash('Fakts veiksmīgi pievienots!');
+	} else {
+        set_flash('Faktu pievienot neizdevās!');
+    }
+    redirect('/' . $category->textid);
+}
+
+
+
+// pievienošanas forma
+if (!isset($_GET['var1'])) {
+    $tpl->newBlock('block-add');
+    $tpl->assign('xsrf', $auth->xsrf);
+}
+
+
+
+// no datubāzes atlasa visus pievienotos konkrētā veida faktus un
+// izvada tos saraksta veidā
+$facts = $db->get_results("SELECT * FROM `rs_facts` WHERE `deleted_by` = 0 ORDER BY `id` DESC");
+
+if ($facts) {
+
+    $counter = 1;
+
+	$tpl->newBlock('block-list');
+
+	foreach ($facts as $fact) {
+    
+        $fact->text = stripslashes($fact->text);
+
+		$tpl->newBlock('single-fact');
+        $tpl->assignAll($fact);
+        $tpl->assign(array(
+            'xsrf' => $auth->xsrf,
+            'counter' => $counter
+        ));
+        
+        $counter++;
+	}
+}
--- exs.lv/modules/rs-facts/rs-facts.tpl
+++ exs.lv/modules/rs-facts/rs-facts.tpl
@@ -1,1 +1,66 @@
- 
+<ul class="tabs">
+	<li><a href="/{category-url}"><span class="pages">RuneScape fakti</span></a></li>
+</ul>
+
+<!-- START BLOCK : block-edit -->
+<h2 style="font-weight:bold">Labot faktu</h2>
+
+<form id="edit-fact" class="form" method="post">
+	<fieldset>
+        <input type="hidden" name="anti-xsrf" value="{xsrf}">
+		<p>
+			<label for="edit-fact">Fakts:</label><br>
+			<textarea id="edit-fact" class="text" name="edit-fact" style="width: 500px; height: 100px;" cols="10" rows="4">
+                {text}
+            </textarea>
+		</p>
+		<p>
+			<input type="submit" id="submit" class="button primary" name="submit" value="Saglabāt">
+		</p>
+	</fieldset>
+</form>
+<!-- START BLOCK : block-edit -->
+
+<!-- START BLOCK : block-add-->
+<h2 style="font-weight:bold">Jauns fakts</h2>
+
+<form id="new-fact" class="form" method="post">
+	<fieldset>
+        <input type="hidden" name="anti-xsrf" value="{xsrf}">
+		<p>
+			<textarea id="new-fact" class="text" name="new-fact" style="width: 500px; height: 100px;" cols="10" rows="4">
+            </textarea>
+		</p>
+		<p>
+			<input type="submit" id="submit" class="button primary" name="submit" value="Pievienot">
+		</p>
+	</fieldset>
+</form>
+<!-- START BLOCK : block-add-->
+
+<!-- START BLOCK : block-list -->
+<table class="main-table facts-table">
+	<tr>
+        <th style="padding:0"></th>
+		<th>Teksts</th>
+		<th>Labot</th>
+		<th>Dzēst</th>
+	</tr>
+	<!-- START BLOCK : single-fact -->
+	<tr>
+        <td class="row-counter">{counter}</td>
+		<td>{text}</td>
+		<td class="row-center">
+            <a href="/{category-url}/edit/{id}">
+                <img src="/bildes/fugue-icons/receipt--pencil.png" title="Labot" alt="">
+            </a>
+        </td>
+		<td class="row-center">
+            <a class="confirm" href="/{category-url}/delete/{id}?val={xsrf}">
+                <img src="/bildes/fugue-icons/cross.png" title="Dzēst" alt="">
+            </a>
+        </td>
+	</tr>
+	<!-- END BLOCK : single-fact -->
+</table>
+<!-- START BLOCK : block-list -->
