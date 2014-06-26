66d849713842f2dbbc9013ab9d42da2e5f96eebd
Madars <mad182@gmail.com>
2014-06-26 12:00:27 +0300

    Lapas kartes struktūra

--- exs.lv/modules/sitemap/sitemap.php
+++ exs.lv/modules/sitemap/sitemap.php
@@ -24,37 +24,6 @@ if ($auth->level == 1 && isset($_GET['movedown'])) {
 	}
 }
 
-function sitemap($parent = 0, $sitemap_modules = 'list') {
-	global $db, $lang, $auth;
-
-	$cats = $db->get_results("SELECT `id`,`textid`,`title`,`content`,`isforum` FROM `cat` WHERE `parent` = '$parent' AND (`lang` = '$lang' OR `lang` = 0) AND `mods_only` = 0 AND `sitemap` = 1 AND `module` IN(" . $sitemap_modules . ") ORDER BY `ordered` ASC");
-
-	$out = '';
-	if ($cats) {
-		$out .= '<ul>';
-		foreach ($cats as $cat) {
-			$out .= '<li>';
-
-			$description = $cat->title;
-			if (!empty($cat->isforum)) {
-				$description = strip_tags($cat->content);
-			}
-
-			if ($auth->level == 1) {
-				$out .= ' <a style="float: right;padding: 0 2px;margin: 0;border: 0" href="?moveup=' . $cat->id . '">&#8593;</a> ';
-				$out .= ' <a style="float: right;padding: 0 2px;margin: 0;border: 0" href="?movedown=' . $cat->id . '">&#8595;</a> ';
-			}
-
-			$out .= '<a href="/' . $cat->textid . '" title="' . htmlspecialchars($description) . '">' . $cat->title . '</a>';
-
-			$out .= sitemap($cat->id, $sitemap_modules);
-			$out .= '</li>';
-		}
-
-		$out .= '</ul>';
-	}
-	return $out;
-}
-
 $out = '';
 $tpl->assign('out', sitemap(0, $sitemap_modules));
+
