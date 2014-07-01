fceaaebd8c5b1afed55b0c987b8e33315e5c140a
edgarspelna <edgar.pelna@gmail.com>
2014-07-01 21:34:58 +0300

    mvc: uzlabotas kontrollera funkcijas, labāka modeļu/submoduļu/skatu ielāde

--- exs.lv/includes/class.controller.php
+++ exs.lv/includes/class.controller.php
@@ -3,16 +3,33 @@
  *  Parent klase MVC arhitektūras kontrolleriem
  *
  *  Kontrolleri, kas šo klasi atvasina, meklējami moduļu mapēs.
- *  Caur šo klasi pēc vajadzības notiek arī modeļa ielāde.
+ *
+ *  TODO:
+ *      - as_classname() - atstarpes jādzēš
+ *      - $this->quit() kā funkcija globālajos failos *
+ *
+ *  Daudzas funkcijas ievadē saņem faila nosaukumu. Ar nosaukumu jāsaprot,
+ *  ka norādīts drīkst būt arī ceļš, sākot no atvērtā moduļa mapes.
+ *
+ *  Nosaukuma piemēri:
+ *      "otherModule"
+ *      "sub-modules/sub_sub_modules/different"
+ *
+ *  Nosaukumā pieļaujamie simboli: 
+ *      A-Z, a-z, 0-9, "_", "-", "/".
  */
 
 class Controller {
-
-    // atsauce uz ielādēto modeli
-    protected $model;
     
     // atsauce uz $tpl globālo mainīgo
     protected $view;
+    
+    // ceļš pa mapēm uz atvērto moduli
+    private $path;
+    
+    // masīvs ar ielādētajiem modeļiem, lai zinātu, 
+    // vai nepieprasa vienu un to pašu atkārtoti
+    private $loaded_models;
     
     /**
      *  Pievieno kontrollerim atsauces uz atsevišķiem 
@@ -33,121 +50,126 @@ class Controller {
 
         global $tpl;
         $this->view =& $tpl;
-
-        $this->model = false;
-    }    
+        $this->path = CORE_PATH.'/modules/'.$this->category->module.'/';
+        
+        $this->loaded_models = array();
+    }
     
     /**
-     *  Ielādē moduļa mapē esošu template failu
+     *  Ielādē norādīto modeli
+     *  
+     *  @param string $file  faila nosaukums
+     */
+    protected function model($file = '') {
+        
+        // nosaukums mainīgajam, caur kādu varēs atsaukties uz modeli
+        $variable_name = trim($file);
+        $allowed = "/[^a-z0-9_]/i";
+        $variable_name = preg_replace($allowed, '', $variable_name);
+
+        if (isset($this->{$variable_name})) {
+            $this->quit('Neizdevās izveidot mainīgo ar šādu modeļa nosaukumu');
+        }
+
+        // faila nosaukuma pārbaudes
+        $file = $this->as_file_name($file);
+        if (empty($file)) $this->quit('Kļūdaini norādīti parametri');
+
+        if (!file_exists($this->path.$file.'.php')) {
+            $this->quit('Norādītais \''.$file.'\' fails neeksistē');
+        }
+        
+        require_once(CORE_PATH . '/includes/class.model.php');
+        require($this->path.$file.'.php');
+        
+        // vienu un to pašu modeli nedrīkst ielādēt atkārtoti;
+        // bez kļūdas paziņojuma arī var iztikt, jo modelis jau ir ielādēts
+        $class_name = as_classname($this->get_last_part($file));
+        if (in_array($class_name, $this->loaded_models)) {
+            return;
+        }
+        
+        // inicializē modeļa objektu
+        $class_name = 'Model_' . $class_name;
+        $this->{$variable_name} = new $class_name();
+    }
+
+    /**
+     *  Atgriež norādīto skatu kā jaunu template objektu
+     *
+     *  (Noder jquery funkcijām, kur nepieciešama tikai neliela skata daļa.)
      *
      *  @param string $file     faila nosaukums
      *  @return TemplatePower   template objekts
      */
     protected function view($file = '') {
         
-        $file = trim($file);        
-        if ($file == '') $this->display_error('Template fails neeksistē');
-
-        $file = CORE_PATH.'/modules/'.$this->category->module.'/'.$file.'.tpl';
-        
-        if (!file_exists($file)) {
-            return false;
+        $file = as_file_name($file);   
+        if (empty($file)) $this->quit('Kļūdaini norādīti parametri');
+     
+        if (!file_exists($this->path.$file.'.tpl')) {
+            $this->quit('Norādītais \''.$file.'\' fails neeksistē');
         }
 
-        $tpl = new TemplatePower($file);
+        $tpl = new TemplatePower($this->path.$file.'.tpl');
         $tpl->prepare();
         
         return $tpl;
-    }    
+    }
     
     /**
-     *  Ielādē moduļa modeli
+     *  Ielādē norādīto skatu parent skatā
      *
-     *  Nosaukumā (arī faila) drīkst būt tikai burti, cipari, "_" un "/".
-     *
-     *  Ja modeļa fails atrodas moduļa apakšmapē, drīkst norādīt ceļu,
-     *  piemēram,
-     *      
-     *      submodels/model_1
-     *          vai
-     *      models/user_profile
-     *  
-     *  @param string $model_name   modeļa nosaukums bez faila paplašinājuma
+     *  @param string $file          faila nosaukums
+     *  @param string $parent_block  bloka nosaukums, kurā saturu ielādēt
      */
-    protected function model($model_string = '') {
-
-        if ($this->model !== false) 
-            $this->display_error('Vienlaicīgi kontrollerī var ielādēt tikai ' . 
-                'vienu modeli!');
-
-        // atstās tikai pieļaujamos simbolus
-        $model_string = $this->escape_model_string($model_string);
-        if (empty($model_string)) return false;
+    protected function subview($file = '', $parent_block = '') {
         
-        // nolasīs pēdējo string'a daļu (aka klases nosaukumu), 
-        // ja norādīts arī ceļš uz modeli
-        $class_name = $model_string;
-        if (($pos = strpos($model_string, '/')) !== false) { 
-
-            $matches = explode('/', $model_string);
-            $last_match = $matches[sizeof($matches) - 1];
-            $last_match = as_classname($last_match);
-
-            if ($last_match === false) 
-                $this->display_error('Nepareizi norādīts modelis!');
-            $class_name = $last_match;
-        }
-
-        require_once(CORE_PATH . '/includes/class.model.php');
-        $file_path = CORE_PATH . '/modules/' . $this->category->module 
-                                 . '/' . $model_string . '.php';
-
-        if (file_exists($file_path)) {
-            require($file_path);
-        } else {
-            $this->display_error('Norādītais modelis neeksistē!');
-        }
-
-        // inicializē modeļa objektu
-        $class_name = 'Model_' . $class_name;        
-        $this->model = new $class_name();
-    }    
-    
-    /**
-     *  Atbrīvo atsauci uz ielādēto modeli, lai varētu ielādēt citu
-     */
-    protected function clear_model() {
-        $this->model = false;
-    }    
-    
-    /**
-     *  Eskeipo modeļa faila nosaukumu
-     *
-     *  Atstās nosaukumā tikai burtus, ciparus, "_" un "/".
-     */
-    private function escape_model_string($string = '') {
-    
-        if (empty($string) || trim($string) === '') 
-            return trim($string);
-
-        // modeļa nosaukumā drīkst būt "/", 
-        // lai varētu norādīt ceļu uz apakšmapi
-        $allowed = "/[^a-z0-9_\/]/i";
-        $string = preg_replace($allowed, '', $string);
+        $file = as_file_name($file);
+        $parent_block = trim($parent_block);
         
-        return $string;
-    } 
+        if (empty($file) || empty($parent_block)) {
+            $this->quit('Kļūdaini norādīti parametri');
+        }
+
+        if (!file_exists($this->path.$file.'.tpl')) {
+            $this->quit('Norādītais \''.$file.'\' fails neeksistē');
+        }
+        
+        $this->view->assignInclude($parent_block, $this->path.$file.'.tpl');
+        $this->view->prepare();
+    }
     
     /**
-     *  Lapas izstrādātājiem būs redzami kļūdas paziņojumi
+     *  Ielādē apakšmoduli un inicializē tajā esošo klasi
+     *
+     *  @param string $file     faila nosaukums
      */
-    private function display_error($string = '') {
-        if ($this->debug) {
-            die('Kļūda: '. $string);
-        } else {
-            die('Ooooops! Sistēmas kļūda. :)');
+    protected function submodule($file = '') {
+
+        $file = as_file_name($file);        
+        if (empty($file)) {
+            $this->quit('Kļūdaini norādīti parametri');
         }
-    }    
+
+        if (!file_exists($this->path.$file.'.php')) {
+            $this->quit('Norādītais \''.$file.'\' fails neeksistē');
+        }
+        
+        require($this->path.$file.'.php');
+
+        $class_name = as_classname($this->get_last_part($file);
+        if (empty($class_name)) {
+            $this->quit('Kļūda klases nosaukumā');
+        }
+            
+        $class = new $classname();
+
+        if (!method_exists($class, 'index')) {
+            $this->quit('Objektu neizdevās inicializēt');
+        }
+        $class->index();
+    }
     
     /**
      *  Pārbauda, vai lietotājs ir tiesīgs skatīt sadaļu
@@ -155,11 +177,57 @@ class Controller {
     protected function check_permission($string = 'mod') {
         if ($string === 'mod' && !im_mod()) {
             set_flash('Pieeja liegta!');
+        } else if ($string === 'ra' && $this->auth->level > 3) {
+            set_flash('Pieeja liegta!');
         } else if ($string === 'user' && !$this->auth->ok) {
             set_flash('Pieeja liegta! Lūdzu, autorizējies.');
-        } else {
-            set_flash('Pieeja liegta!');
         }
         redirect();
+    }
+    
+    /**
+     *  Apstrādā faila nosaukumu atbilstoši šīs klases formātam,
+     *  dzēšot neatļautos simbolus
+     *
+     *  @param string $file_name    faila nosaukums
+     */
+    private function as_file_name($file_name = '') {
+
+        $file_name = trim($file_name);
+        $allowed = "/[^a-z0-9_\-\/]/i";
+        $file_name = preg_replace($allowed, '', $file_name);
+        
+        return $file_name;
+    }
+    
+    /**
+     *  No virknes formātā <nosaukums>/<nosaukums>/.. atgriež pēdējo daļu
+     *
+     *  @param string $path_string  faila nosaukums
+     */
+    private function get_last_part($path_string = '') {
+    
+        if (empty($path_string)) return '';
+
+        if (($pos = strpos($path_string, '/')) !== false) {
+            $matches = explode('/', $path_string);
+            $path_string = $matches[sizeof($matches) - 1];
+        }
+        
+        return $path_string;
+    }
+    
+    /**
+     *  Atgriezīs kļūdas paziņojumu un pārtrauks satura ielādi
+     *
+     *  Ja lietotājs nav administrators, drošības apsvērumu dēļ
+     *  norādīto paziņojumu neredzēs.
+     */
+    private function quit($string = '') {
+        if ($this->debug) {
+            die('Kļūda: '. $string);
+        } else {
+            die('Ooooops! Sistēmas kļūda. :)');
+        }
     }
 }
