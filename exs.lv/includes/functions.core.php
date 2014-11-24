f9d12e6f13d19cd941b168ebf2f8f5246fd0bda3
Madars <mad182@gmail.com>
2014-11-24 10:55:21 +0200

    Vecaku php versiju atbalsts

--- exs.lv/includes/functions.core.php
+++ exs.lv/includes/functions.core.php
@@ -51,6 +51,110 @@ function as_class_name($name = '') {
 }
 
 /**
+ * For legacy php versions
+ */
+if (!function_exists('http_response_code')) {
+
+	function http_response_code($code = NULL) {
+
+		if ($code !== NULL) {
+
+			switch ($code) {
+				case 100: $text = 'Continue';
+					break;
+				case 101: $text = 'Switching Protocols';
+					break;
+				case 200: $text = 'OK';
+					break;
+				case 201: $text = 'Created';
+					break;
+				case 202: $text = 'Accepted';
+					break;
+				case 203: $text = 'Non-Authoritative Information';
+					break;
+				case 204: $text = 'No Content';
+					break;
+				case 205: $text = 'Reset Content';
+					break;
+				case 206: $text = 'Partial Content';
+					break;
+				case 300: $text = 'Multiple Choices';
+					break;
+				case 301: $text = 'Moved Permanently';
+					break;
+				case 302: $text = 'Moved Temporarily';
+					break;
+				case 303: $text = 'See Other';
+					break;
+				case 304: $text = 'Not Modified';
+					break;
+				case 305: $text = 'Use Proxy';
+					break;
+				case 400: $text = 'Bad Request';
+					break;
+				case 401: $text = 'Unauthorized';
+					break;
+				case 402: $text = 'Payment Required';
+					break;
+				case 403: $text = 'Forbidden';
+					break;
+				case 404: $text = 'Not Found';
+					break;
+				case 405: $text = 'Method Not Allowed';
+					break;
+				case 406: $text = 'Not Acceptable';
+					break;
+				case 407: $text = 'Proxy Authentication Required';
+					break;
+				case 408: $text = 'Request Time-out';
+					break;
+				case 409: $text = 'Conflict';
+					break;
+				case 410: $text = 'Gone';
+					break;
+				case 411: $text = 'Length Required';
+					break;
+				case 412: $text = 'Precondition Failed';
+					break;
+				case 413: $text = 'Request Entity Too Large';
+					break;
+				case 414: $text = 'Request-URI Too Large';
+					break;
+				case 415: $text = 'Unsupported Media Type';
+					break;
+				case 500: $text = 'Internal Server Error';
+					break;
+				case 501: $text = 'Not Implemented';
+					break;
+				case 502: $text = 'Bad Gateway';
+					break;
+				case 503: $text = 'Service Unavailable';
+					break;
+				case 504: $text = 'Gateway Time-out';
+					break;
+				case 505: $text = 'HTTP Version not supported';
+					break;
+				default:
+					exit('Unknown http status code "' . htmlentities($code) . '"');
+					break;
+			}
+
+			$protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
+
+			header($protocol . ' ' . $code . ' ' . $text);
+
+			$GLOBALS['http_response_code'] = $code;
+		} else {
+
+			$code = (isset($GLOBALS['http_response_code']) ? $GLOBALS['http_response_code'] : 200);
+		}
+
+		return $code;
+	}
+
+}
+
+/**
  *  Atgriež objektu ar atvērtā moduļa template failu
  *
  *  Noder reizēs, kad ajax pieprasījumam jāatgriež html saturs. To var 
@@ -58,12 +162,13 @@ function as_class_name($name = '') {
  */
 function fetch_tpl() {
 	global $category;
-	
-	if (empty($category->module)) return false;
-	
+
+	if (empty($category->module))
+		return false;
+
 	$tpl = new TemplatePower(CORE_PATH . '/modules/' . $category->module . '/' . $category->module . '.tpl');
 	$tpl->prepare();
-	
+
 	return $tpl;
 }
 
@@ -2236,35 +2341,35 @@ function profile_menu($user, $active, $title, $action = null) {
 /**
  * Atgriež pieprasītā domēna ($site_id) izmantoto protokolu (http/https)
  */
- function get_protocol($site_id = 1) {
- 	global $config_domains;
+function get_protocol($site_id = 1) {
+	global $config_domains;
 
 	if (!empty($config_domains[$site_id]['ssl'])) {
 		return 'https://';
 	}
 
 	return 'http://';
- }
+}
 
 /**
  * Atgriež domēnu pēc site_id ($lang)
  */
- function get_domain($site_id = 1) {
- 	global $config_domains;
+function get_domain($site_id = 1) {
+	global $config_domains;
 	return $config_domains[$site_id]['domain'];
- }
- 
+}
+
 /**
  * Dateks.lv reklāma, dabū xml
  */
- function get_dateks_xml() {
+function get_dateks_xml() {
 	global $m;
 
 	if (!($data = $m->get('dateks_xml'))) {
 		$data = curl_get("http://www.dateks.lv/rss/new_products.php");
 		$m->set('dateks_xml', $data, false, 1000);
 	}
-	return  simplexml_load_string($data);
+	return simplexml_load_string($data);
 }
 
 /**
@@ -2272,23 +2377,23 @@ function profile_menu($user, $active, $title, $action = null) {
  */
 function show_dateks_view() {
 	global $m;
-	
+
 	if (isset($_GET['pg'])) {
 		$id = (int) $_GET['pg'];
 	} else {
 		$id = 1;
 	}
-	
+
 	if (!($text = $m->get('dateks_pge_' . $id))) {
 
 		$data = get_dateks_xml();
 
 		$text = '';
 		$i = 0;
-		foreach($data->channel->item as $item) {
+		foreach ($data->channel->item as $item) {
 
 			$i++;
-			if($i != $id) {
+			if ($i != $id) {
 				continue;
 			}
 
@@ -2305,8 +2410,8 @@ function show_dateks_view() {
 			$text .= '</p>';
 
 			$item->description = str_replace('align="left"', 'class="dateks-productimage"', $item->description);
-			$text .= filterb4db('<div class="dateks-content"><a href="'.$item->link.'">'. $item->description.'</a></div>');
-		
+			$text .= filterb4db('<div class="dateks-content"><a href="' . $item->link . '">' . $item->description . '</a></div>');
+
 			$text = str_replace('http://www.dateks.lv/images/', 'https://www.dateks.lv/images/', $text);
 
 			require_once(LIB_PATH . '/htmlpurifier/library/HTMLPurifier.includes.php');
@@ -2324,12 +2429,10 @@ function show_dateks_view() {
 			$text = str_replace(' dateksaa ', ' <a href="http://www.dateks.lv/ref/view.html">dateksā</a> ', $text);
 			$text = str_replace('dateks.lv/cenas', 'dateks.lv/p/view/cenas', $text);
 		}
-		
+
 		$m->set('dateks_pge_' . $id, $text, false, 60);
-		
 	}
 
 	return add_smile($text);
 }
 
- 
