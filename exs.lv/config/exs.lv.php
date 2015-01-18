14ff13f43801dd7ef704ca53091a37a7f782627f
Madars <mad182@gmail.com>
2015-01-18 13:50:21 +0200

    Steam api config

--- exs.lv/config/exs.lv.php
+++ exs.lv/config/exs.lv.php
@@ -8,6 +8,11 @@ $fb_api_key = 'f6ac0e495e8b5a09ff2ea463383dc57c';
 $dr_api_id = 15005147;
 $dr_api_key = 'f38c225b8f65df03c5aaa847b1f052a9';
 
+//steam login
+$steam_api_key = "D92CAC5D0E6086FAD16936C2B644EFDA"; //API atslēga
+$steam_domain_name = "exs.lv"; //domēns, kas rādas steam lapā
+$steam_login_page = "https://exs.lv/steam-login"; //uz kurieni redirektēt pēc logina
+
 //twitter login
 $CONSUMER_KEY = 'r7Wjk5VoxlMVdDrkK7wN3X6q2';
 $CONSUMER_SECRET = 'axdLXZy6tm5pM1nB4VM2IZ9UvZKotH22xZlrvNPOKkge86UFen';
@@ -39,3 +44,4 @@ if ($_SERVER['SERVER_NAME'] !== 'localhost' && substr($_SERVER['SERVER_NAME'], 0
  * exs.lv specific functions
  */
 require(CORE_PATH . '/includes/functions.exs.php');
+
