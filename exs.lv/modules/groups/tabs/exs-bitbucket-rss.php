33725b157ea50f7c4b1aa255c674f6e2f5b12ffb
Madars <mad182@gmail.com>
2015-01-19 16:34:05 +0200

    Group tabs

--- exs.lv/modules/groups/tabs/exs-bitbucket-rss.php
+++ exs.lv/modules/groups/tabs/exs-bitbucket-rss.php
@@ -1,15 +1,10 @@
 <?php
 
-if ($auth->ok) {
-
-	if ($file = simplexml_load_file('https://bitbucket.org/mad182/exs-lv/rss?token=a74189d9ab66cb0535a36fe89869a012')) {
-		$module_content = '<ul style="padding: 20px 50px;margin: 0;">';
-		foreach ($file->channel->item as $item) {
-			$module_content .= '<li class="mbox">' . date('Y-m-d', strtotime($item->pubDate)) . ' <a href="' . $item->link . '">' . $item->title . '</a></li>';
-		}
-		$module_content .= '</ul>';
+if ($file = simplexml_load_file('https://bitbucket.org/mad182/exs-lv/rss?token=a74189d9ab66cb0535a36fe89869a012')) {
+	$module_content = '<ul style="padding: 20px 50px;margin: 0">';
+	foreach ($file->channel->item as $item) {
+		$module_content .= '<li class="mbox">' . date('Y-m-d', strtotime($item->pubDate)) . ' <a href="' . $item->link . '">' . $item->title . '</a></li>';
 	}
-} else {
-	$module_content = 'Log in';
+	$module_content .= '</ul>';
 }
 
