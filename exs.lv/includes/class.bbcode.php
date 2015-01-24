b3e14e3e9742ca14290b3c7ac4fae9ef6dbef470
edgarspelna <edgar.pelna@gmail.com>
2015-01-24 21:08:39 +0200

    fix'd BBcode klases "array to string conversion" erroru

--- exs.lv/includes/class.bbcode.php
+++ exs.lv/includes/class.bbcode.php
@@ -1647,7 +1647,7 @@ class BBCode {
 // Make urls clickable
 	function process_urls() {
 		// characters allowed in email
-		$chars = array();
+		$chars = '';
 		for ($i = 224; $i < 256; $i++) {
 			if ($i != 247) {
 				$chars .= chr($i);
