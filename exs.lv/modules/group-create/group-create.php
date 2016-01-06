32b6eb008a161c0b629886d9e9bf5bd42144762a
Madars <mad182@gmail.com>
2016-01-06 11:39:43 +0200

    Pie grupas izveides pārbauda, vai tāda grupa jau neeksistē

--- exs.lv/modules/group-create/group-create.php
+++ exs.lv/modules/group-create/group-create.php
@@ -16,10 +16,15 @@ if ($auth->ok) {
 	$credit = $db->get_var("SELECT credit FROM users WHERE id = '$auth->id'");
 
 	if (isset($_POST['new-title']) && !empty($_POST['new-title'])) {
-		if ($credit < 3) {
+		$title = sanitize(h(trim($_POST['new-title'])));
+
+		if($db->get_var("SELECT count(*) FROM `clans` WHERE `title` = '".$title."'")) {
+			set_flash('Izvēlies citu nosaukumu, šāda grupa jau eksistē!', 'error');
+
+		} elseif ($credit < 3) {
 			set_flash('Nepietiek exs punktu!', 'error');
 		} else {
-			$title = sanitize(h(trim($_POST['new-title'])));
+
 			$db->query("UPDATE users SET credit = credit-'3' WHERE id = ('" . $auth->id . "')");
 			$db->query("INSERT INTO clans (title,date_created,owner,lang) VALUES ('$title','" . time() . "','$auth->id','$lang')");
 			update_karma($auth->id, true);
