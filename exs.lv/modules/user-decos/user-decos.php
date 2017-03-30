8cc11e63e63227933150bc72f6d577ebc85a0feb
Alberts Saulitis <alberts@exs.lv>
2017-03-31 02:52:03 +0300

    user-decos: Iespēja pievienot visiem lietotājiem apbalvojumu ikonas un noņemt tās

--- exs.lv/modules/user-decos/user-decos.php
+++ exs.lv/modules/user-decos/user-decos.php
@@ -7,6 +7,56 @@ if (!im_mod()) {
 	redirect();
 }
 
+global $m;
+
+
+if (isset($_POST['give_deco_all'])) {
+    if ($m->get('give_deco_all') === false) {
+        $decosAll = array();
+        if ($handle = opendir('bildes/fugue-icons/')) {
+            while (false !== ($file = readdir($handle))) {
+                if ($file != "." && $file != "..") {
+                    $allDecos[] = "/bildes/fugue-icons/" . $file;
+                }
+            }
+            closedir($handle);
+        }
+        $decoUsers = $db->get_results("SELECT id FROM users where decos='' AND lastseen BETWEEN DATE_SUB(NOW(), INTERVAL 30 DAY) AND NOW()");
+        foreach ($decoUsers as $decoUser){
+            $decos = array();
+            $decos[] = [
+                'title' => h(''),
+                'icon' => h($allDecos[rand(0, sizeof($allDecos)-1)])
+            ];
+            $db->query("UPDATE `users` SET `decos` = '" . sanitize(serialize($decos)) . "' WHERE `id` = '$decoUser->id'");
+        }
+
+        $auth->log('Pievienoja visiem lietotājiem profila ikonu', 'users');
+        echo 'a';
+        $m->set('give_deco_all', 1, 600);
+    }
+    else {
+        $auth->log('Neveiksmīgi mēģināja pievienot visiem lietotājiem profila ikonu', 'users');
+    }
+
+}
+
+if (isset($_POST['remove_deco_all'])) {
+    if ($m->get('remove_deco_all') === false) {
+        $new = sanitize(serialize(array()));
+        $db->query("UPDATE `users` SET `decos` = '$new'");
+        $auth->log('Noņēma profila ikonas visiem', 'users');
+
+        $m->set('remove_deco_all', 1, 600);
+    }
+    else {
+        $auth->log('Neveiksmīgi mēģināja noņemt visiem lietotājiem profila ikonu', 'users');
+    }
+
+}
+
+
+
 if (isset($_POST['userid']) && isset($_POST['title']) && isset($_POST['icon'])) {
 
 	if(substr($_POST['icon'],0,8) !== 'https://') {
@@ -39,7 +89,7 @@ if (isset($_POST['userid']) && isset($_POST['title']) && isset($_POST['icon'])) 
 	}
 }
 
-$listdecos = $db->get_results("SELECT * FROM `users` WHERE `decos` != ''");
+$listdecos = $db->get_results("SELECT * FROM `users` WHERE `decos` != '' LIMIT 100");
 
 foreach ($listdecos as $decos) {
 	$images = unserialize($decos->decos);
@@ -52,7 +102,6 @@ foreach ($listdecos as $decos) {
 					'icon' => $deco['icon'],
 				];
 			}
-
 			$tpl->newBlock('existing-deco');
 			$tpl->assign([
 				'title' => $deco['title'],
