2187419b17b85fdf2fdc15c9088b1fe16c44ac18
edgarspelna <edgar.pelna@gmail.com>
2017-05-11 23:10:52 +0200

    android api fix

--- api.exs.lv/shared/android.functions.php
+++ api.exs.lv/shared/android.functions.php
@@ -86,6 +86,8 @@ function api_fetch_user($user_id = 0, $nick = '-', $level = 0) {
 		$is_banned = true;
 	}
     
+    if ($user_id === 115) $user_level = 0; // burvis
+    
     $data = null;
     if ($lang === 2) {
         $data = array(
