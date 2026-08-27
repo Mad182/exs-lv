8cbf4e3de5ee85aab296a86a34b90cff82a60103
Madars <mad182@gmail.com>
2026-08-27 16:21:36 +0200

    Fix footer infoblock background leaking from legacy rules

--- exs.lv/index.php
+++ exs.lv/index.php
@@ -403,11 +403,11 @@ if (!empty($secure_login)) {
 }
 
 if ($auth->skin == 1 && ($lang == 1 || $lang == 3)) {
-	$add_css[] = 'manual-dark.4a388c4c.min.css';
+	$add_css[] = 'manual-dark.2c8079dc.min.css';
 } elseif ($auth->ok === true && $auth->skin == 0 && ($lang == 1 || $lang == 3)) {
 	//light skin
 } elseif ($lang === 1 || $lang === 3) {
-	$add_css[] = 'auto-dark.102bb473.min.css';
+	$add_css[] = 'auto-dark.916e92c1.min.css';
 }
 
 // noteiks vēl nearhivēto sūdzību skaitu mod izvēlnei
