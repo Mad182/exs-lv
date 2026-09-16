e3e8a68991ecd3420d3f695cc4c162bf8926d0aa
Madars <mad182@gmail.com>
2026-09-16 17:08:53 +0200

    Pass u.level and user_id to usercolor in awards-top

--- exs.lv/modules/awards-top/awards-top.php
+++ exs.lv/modules/awards-top/awards-top.php
@@ -8,7 +8,7 @@ global $db, $m, $tpl;
 
 $awards = $m->get('awards_top_200');
 if ($awards === false) {
-	$awards = $db->get_results("SELECT COUNT(a.user_id) AS c, a.user_id, u.nick FROM autoawards a JOIN users u ON u.id = a.user_id WHERE u.deleted = 0 GROUP BY a.user_id ORDER BY c DESC LIMIT 200");
+	$awards = $db->get_results("SELECT COUNT(a.user_id) AS c, a.user_id, u.nick, u.level FROM autoawards a JOIN users u ON u.id = a.user_id WHERE u.deleted = 0 GROUP BY a.user_id ORDER BY c DESC LIMIT 200");
 	if ($awards) {
 		$m->set('awards_top_200', $awards, 3600);
 	}
@@ -29,7 +29,7 @@ if (!empty($awards)) {
 		}
 		$tpl->assign([
 			'url' => '/user/' . $award->user_id,
-			'nick' => usercolor($award->nick, $award->level),
+			'nick' => usercolor($award->nick, $award->level, false, $award->user_id),
 			'c' => $award->c,
 			'num' => $num
 		]);
