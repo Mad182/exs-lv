b0e4ea8c957731949b8781e872d1eaa30dbe4c01
Madars <mad182@gmail.com>
2026-09-16 17:05:50 +0200

    Optimize /medalas (awards-top) with single JOIN and 1-hour Memcached caching

--- exs.lv/modules/awards-top/awards-top.php
+++ exs.lv/modules/awards-top/awards-top.php
@@ -4,24 +4,33 @@
  * Saņemto medaļu tops -
  * lietotāji ar visvairāk medaļām
  */
-$awards = $db->get_results("SELECT COUNT(user_id) AS c, user_id FROM autoawards GROUP BY user_id ORDER BY c DESC LIMIT 200");
+global $db, $m, $tpl;
+
+$awards = $m->get('awards_top_200');
+if ($awards === false) {
+	$awards = $db->get_results("SELECT COUNT(a.user_id) AS c, a.user_id, u.nick FROM autoawards a JOIN users u ON u.id = a.user_id WHERE u.deleted = 0 GROUP BY a.user_id ORDER BY c DESC LIMIT 200");
+	if ($awards) {
+		$m->set('awards_top_200', $awards, 3600);
+	}
+}
 
 $i = 1;
 $num = 1;
 $count = 0;
 
-foreach ($awards as $award) {
-	$tpl->newBlock('aw-top');
-	if ($award->c != $count) {
-		$num = $i;
-		$count = $award->c;
+if (!empty($awards)) {
+	foreach ($awards as $award) {
+		$tpl->newBlock('aw-top');
+		if ($award->c != $count) {
+			$num = $i;
+			$count = $award->c;
+		}
+		$tpl->assign([
+			'nick' => $award->nick,
+			'c' => $award->c,
+			'num' => $num
+		]);
+		$i++;
 	}
-	$user = get_user($award->user_id);
-	$tpl->assign([
-		'nick' => $user->nick,
-		'c' => $award->c,
-		'num' => $num
-	]);
-	$i++;
 }
 
