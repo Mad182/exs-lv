574ca7240b3e2d39c4bf53e9b8997dbcf1c83d87
Madars <mad182@gmail.com>
2026-09-16 17:11:11 +0200

    Optimize /blogstats by removing synchronous update_blog_stats and caching query

--- exs.lv/modules/blogstats/blogstats.php
+++ exs.lv/modules/blogstats/blogstats.php
@@ -4,8 +4,7 @@
  * Blogu ierakstu statistika
  */
  
-//updato kategoriju statistiku
-update_blog_stats();
+global $db, $m, $tpl;
 
 //kārtošana
 $ord = 'stat_topics';
@@ -17,17 +16,27 @@ if (isset($_GET['order'])) {
 	}
 }
 
-//parāda tās sadaļas, kurās ir viesmaz viens ieraksts
-$blogs = $db->get_results("SELECT * FROM `cat` WHERE `isblog` != '0' AND `stat_topics` > 0 ORDER BY " . $ord . " DESC");
-foreach ($blogs as $blog) {
-
-	$tpl->newBlock('bs-list-node');
-	$tpl->assign([
-		'url' => '/' . $blog->textid,
-		'title' => $blog->title,
-		'p_count' => $blog->stat_topics,
-		'c_count' => $blog->stat_com,
-		'w_count' => $blog->stat_views
-	]);
+//parāda tās sadaļas, kurās ir vismaz viens ieraksts
+$cache_key = 'blogstats_' . $ord;
+$blogs = $m->get($cache_key);
+if ($blogs === false) {
+	$blogs = $db->get_results("SELECT * FROM `cat` WHERE `isblog` != '0' AND `stat_topics` > 0 ORDER BY " . $ord . " DESC");
+	if ($blogs) {
+		$m->set($cache_key, $blogs, 1800);
+	}
 }
+
+if (!empty($blogs)) {
+	foreach ($blogs as $blog) {
+		$tpl->newBlock('bs-list-node');
+		$tpl->assign([
+			'url' => '/' . $blog->textid,
+			'title' => $blog->title,
+			'p_count' => $blog->stat_topics,
+			'c_count' => $blog->stat_com,
+			'w_count' => $blog->stat_views
+		]);
+	}
+}
+
 
